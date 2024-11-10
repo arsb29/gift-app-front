a93bd3750112ae8fda1760e4d53e3660be8e6a9c
ar.a.belyaev <ar.a.belyaev@tinkoff.ru>
2024-11-10 21:49:10 +0300

    обновлен formatNameShort

--- src/helpers/formatName.ts
+++ src/helpers/formatName.ts
@@ -8,6 +8,8 @@ export function formatName(user?: User | null) {
 
 export function formatNameShort(user: User) {
   if (!user) return null;
+  if (!(user.firstName || user.lastName) && user.username)
+    return user.username[0];
   return [user.firstName, user.lastName]
     .filter(Boolean)
     .map((name) => name[0])
