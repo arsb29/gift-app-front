9aa2e1f58d9ab1f153033d59a611793409043b59
ar.a.belyaev <ar.a.belyaev@tinkoff.ru>
2024-11-10 11:58:30 +0300

    если нет имени выводить username

--- src/helpers/formatName.ts
+++ src/helpers/formatName.ts
@@ -2,6 +2,7 @@ import { User } from "@/types.ts";
 
 export function formatName(user?: User | null) {
   if (!user) return null;
+  if (!(user.firstName || user.lastName) && user.username) return user.username;
   return [user.firstName, user.lastName].filter(Boolean).join(" ");
 }
 
